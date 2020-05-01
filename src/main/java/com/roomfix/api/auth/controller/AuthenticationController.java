598a515919b43274f32db94a04b77fa2629375c2
Alvin J. Alexander <apouret@juniorisep.com>
2020-05-01 15:03:49 +0200

    added JWT login for every endpoint ecxcept /account/*

--- src/main/java/com/roomfix/api/auth/controller/AuthenticationController.java
+++ src/main/java/com/roomfix/api/auth/controller/AuthenticationController.java
@@ -1,11 +1,16 @@
 package com.roomfix.api.auth.controller;
 
+import com.roomfix.api.auth.component.JwtUtil;
 import com.roomfix.api.auth.dto.LoginRequestDto;
 import com.roomfix.api.auth.dto.LoginResponseDto;
 import com.roomfix.api.auth.dto.UserCreateDto;
 import com.roomfix.api.common.exceptionhandling.exception.BadRequestException;
+import com.roomfix.api.common.exceptionhandling.exception.InternalServerErrorException;
+import com.roomfix.api.common.exceptionhandling.exception.UnauthorizedException;
 import com.roomfix.api.user.entity.User;
 import com.roomfix.api.user.entity.UserRole;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
 import com.roomfix.api.user.repository.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,20 +24,24 @@ import org.springframework.web.bind.annotation.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/account")
 public class AuthenticationController {
     private UserRepository userRepository;
     private PasswordEncoder passwordEncoder;
     private AuthenticationManager authManager;
+    private JwtUtil jwtTokenUtil;
     private ModelMapper modelMapper;
 
     @Autowired
-    public AuthenticationController(UserRepository userRepository, AuthenticationManager authUserService,  PasswordEncoder passwordEncoder, ModelMapper modelMapper) {
+    public AuthenticationController(UserRepository userRepository, AuthenticationManager authUserService,
+                                    PasswordEncoder passwordEncoder, ModelMapper modelMapper, JwtUtil jwtTokenUtil) {
         this.userRepository = userRepository;
         this.authManager = authUserService;
         this.passwordEncoder = passwordEncoder;
         this.modelMapper = modelMapper;
+        this.jwtTokenUtil = jwtTokenUtil;
     }
 
     @PostMapping("/register")
@@ -57,20 +66,44 @@ public class AuthenticationController {
         user.setRole(UserRole.STUDENT);
         user.setPassword(passwordEncoder.encode(userCreateDto.getPassword()));
 
-        modelMapper.map(userRepository.save(user), response);
+        this.modelMapper.map(userRepository.save(user), response);
         return response;
+    }
+
+    @PostMapping("/login")
+    public LoginResponseDto login(@RequestBody LoginRequestDto loginRequestDto) {
+        try {
+            this.authenticate(loginRequestDto.getMail(), loginRequestDto.getPassword());
+
+            final User user = userRepository.findByMail(loginRequestDto.getMail());
+            final String token = jwtTokenUtil.generateToken(user);
+
+            LoginResponseDto response = new LoginResponseDto();
+            this.modelMapper.map(user, response);
+            response.setToken(token);
+
+            return response;
+        } catch (DisabledException e1) {
+            throw new UnauthorizedException("Account disabled");
+        } catch (BadCredentialsException e2) {
+            throw new UnauthorizedException("Invalid credentials");
+        } catch (Exception e) {
+            throw new InternalServerErrorException();
+        }
     }
 
     @PostMapping("/check-credentials")
     public ResponseEntity<?> checkCredentials(@RequestBody LoginRequestDto loginRequestDto) {
         try {
-            this.authManager.authenticate(new UsernamePasswordAuthenticationToken(
-                    loginRequestDto.getMail(),
-                    loginRequestDto.getPassword())).isAuthenticated();
+            this.authenticate(loginRequestDto.getMail(), loginRequestDto.getPassword());
             return ResponseEntity.ok("All good!");
-        } catch (Exception e){
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid Account and/or Credentials.");
         }
+    }
+
+    private void authenticate(String email, String password) throws Exception {
+        this.authManager.authenticate(new UsernamePasswordAuthenticationToken(email, password)).isAuthenticated();
     }
 
 }
--- src/main/java/com/roomfix/api/common/configuration/WebSecurityConfig.java
+++ src/main/java/com/roomfix/api/common/configuration/WebSecurityConfig.java
@@ -1,6 +1,8 @@
 package com.roomfix.api.common.configuration;
 
 
+import com.roomfix.api.auth.component.JwtAuthenticationEntryPoint;
+import com.roomfix.api.auth.component.JwtRequestFilter;
 import com.roomfix.api.auth.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -17,8 +19,18 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
     private AuthenticationService authUserService;
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private JwtRequestFilter jwtRequestFilter;
+
+    @Autowired
+    public WebSecurityConfig(AuthenticationService authUserService,
+                             JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+                             JwtRequestFilter jwtRequestFilter) {
+        this.authUserService = authUserService;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -42,16 +54,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable()
                 // dont authenticate this particular request
-                .authorizeRequests().antMatchers("/account/**").permitAll();
+                .authorizeRequests().antMatchers("/account/**").permitAll().
                 // all other requests need to be authenticated
-//                        anyRequest().authenticated().and().
-                // make sure we use stateless session; session won't be used to
-                // store user's state.
-//                        exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        // Add a filter to validate the tokens with every request
-//        httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-//        httpSecurity.cors().and();
+                        anyRequest().authenticated().and().
+//                 make sure we use stateless session; session won't be used to
+//                 store user's state.
+                        exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//         Add a filter to validate the tokens with every request
+        httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        httpSecurity.cors().and();
 
     }
 }
