11ee0cd1987fa7c9513bb364018ae6100cb97684
NWagnerIsep <n-wagner@outlook.com>
2020-05-25 18:44:47 +0100

    refactorisation pour room par id, batiment par id et toutes les rooms

--- src/main/java/com/roomfix/api/failureStatistics/FailureStatisticsController.java
+++ src/main/java/com/roomfix/api/failureStatistics/FailureStatisticsController.java
@@ -19,87 +19,82 @@ import java.util.List;
 @RestController
 @RequestMapping("/statistics")
 public class FailureStatisticsController {
-
     private final BuildingRepository buildingRepository;
-
     private final FailureRepository failureRepository;
-
     private final RoomRepository roomRepository;
 
 
-
     @Autowired
-    public FailureStatisticsController(RoomRepository roomRepository, BuildingRepository buildingRepository, FailureRepository failureRepository){
+    public FailureStatisticsController(RoomRepository roomRepository, BuildingRepository buildingRepository, FailureRepository failureRepository) {
         this.buildingRepository = buildingRepository;
-        this.failureRepository=failureRepository;
-        this.roomRepository=roomRepository;
+        this.failureRepository = failureRepository;
+        this.roomRepository = roomRepository;
     }
-
 
     @GetMapping("/building/summary")
     @ResponseStatus(HttpStatus.OK)
-    public ArrayList<StatisticsDto> getSummaryForAllBuildings(){
+    public ArrayList<StatisticsDto> getSummaryForAllBuildings() {
         return getSummary("building", false, 0);
     }
 
     @GetMapping("/building/{id}/summary")
     @ResponseStatus(HttpStatus.OK)
-    public ArrayList<StatisticsDto> getSummaryForBuilding(@PathVariable("id") long buildingId){
+    public ArrayList<StatisticsDto> getSummaryForBuilding(@PathVariable("id") long buildingId) {
         return getSummary("building", true, buildingId);
     }
 
     @GetMapping("/room/summary")
     @ResponseStatus(HttpStatus.OK)
-    public ArrayList<StatisticsDto> getSummaryForAllRooms(){
+    public ArrayList<StatisticsDto> getSummaryForAllRooms() {
         return getSummary("room", false, 0);
     }
 
     @GetMapping("/room/{id}/summary")
     @ResponseStatus(HttpStatus.OK)
-    public ArrayList<StatisticsDto> getSummaryForRoom(@PathVariable("id") long roomId){
+    public ArrayList<StatisticsDto> getSummaryForRoom(@PathVariable("id") long roomId) {
         return getSummary("room", true, roomId);
     }
 
-    public ArrayList<StatisticsDto> getSummary(String type, boolean byId, long id){
+    private ArrayList<StatisticsDto> getSummary(String type, boolean byId, long id) {
         ArrayList<StatisticsDto> statisticsDtoArrayList = new ArrayList<>();
         List<Failure> failureList = this.failureRepository.findAll();
-        if (type.equals("building" ) && !byId){
+        if (type.equals("building") && !byId) {
             List<Building> buildingList = this.buildingRepository.findAll();
-            for (Building building: buildingList
+            for (Building building : buildingList
             ) {
                 StatisticsDto statisticsDto = new StatisticsDto();
                 long buildingId = building.getId();
                 statisticsDto.setDtoname(building.getName());
-                failuresCount(statisticsDto,failureList,buildingId,true);
+                failuresCount(statisticsDto, failureList, buildingId, true);
                 statisticsDtoArrayList.add(statisticsDto);
             }
         }
-        if (type.equals("room") && !byId){
+        if (type.equals("room") && !byId) {
             List<Room> roomList = this.roomRepository.findAll();
-            for (Room room:roomList
-                 ) {
+            for (Room room : roomList
+            ) {
                 StatisticsDto statisticsDto = new StatisticsDto();
                 long roomId = room.getId();
                 statisticsDto.setDtoname(room.getNumber());
-                failuresCount(statisticsDto,failureList,roomId,false);
+                failuresCount(statisticsDto, failureList, roomId, false);
                 statisticsDtoArrayList.add(statisticsDto);
 
             }
         }
-        if (type.equals("building") && byId){
+        if (type.equals("building") && byId) {
             Building building = this.buildingRepository.findById(id).orElseThrow(ResourceNotFoundException::new);
             StatisticsDto statisticsDto = new StatisticsDto();
             statisticsDto.setDtoname(building.getName());
-            failuresCount(statisticsDto,failureList,id,true);
+            failuresCount(statisticsDto, failureList, id, true);
             statisticsDtoArrayList.add(statisticsDto);
 
         }
 
-        if (type.equals("room") && byId){
+        if (type.equals("room") && byId) {
             Room room = this.roomRepository.findById(id).orElseThrow(ResourceNotFoundException::new);
             StatisticsDto statisticsDto = new StatisticsDto();
             statisticsDto.setDtoname(room.getNumber());
-            failuresCount(statisticsDto,failureList,id,false);
+            failuresCount(statisticsDto, failureList, id, false);
             statisticsDtoArrayList.add(statisticsDto);
 
         }
@@ -107,53 +102,41 @@ public class FailureStatisticsController {
         return statisticsDtoArrayList;
     }
 
-
-    public void failuresCount(StatisticsDto statisticsDto, List<Failure> failureList, long elementId, boolean elementIsBuilding){
+    private void failuresCount(StatisticsDto statisticsDto, List<Failure> failureList, long elementId, boolean elementIsBuilding) {
         long failurePlacementId;
-        for (Failure failure:failureList){
-
-            if (elementIsBuilding){
-                failurePlacementId=failure.getRoom().getBuilding().getId();
+        for (Failure failure : failureList) {
+            if (elementIsBuilding) {
+                failurePlacementId = failure.getRoom().getBuilding().getId();
             } else {
-                failurePlacementId=failure.getRoom().getId();
+                failurePlacementId = failure.getRoom().getId();
             }
-
-            if (failurePlacementId==elementId){
-                switch (failure.getState()){
+            if (failurePlacementId == elementId) {
+                switch (failure.getState()) {
                     case UN_RESOLVED:
                         int newUnresolvedCount = statisticsDto.getUnresolvedFailuresSum();
-                        newUnresolvedCount+=1;
+                        newUnresolvedCount += 1;
                         statisticsDto.setUnresolvedFailuresSum(newUnresolvedCount);
                         break;
 
-
                     case ONGOING:
                         int newOngoingCount = statisticsDto.getOnGoingFailuresSum();
-                        newOngoingCount+=1;
+                        newOngoingCount += 1;
                         statisticsDto.setOnGoingFailuresSum(newOngoingCount);
                         break;
 
                     case CLOSED:
                         int newClosedCount = statisticsDto.getClosedFailuresSum();
-                        newClosedCount+=1;
+                        newClosedCount += 1;
                         statisticsDto.setClosedFailuresSum(newClosedCount);
                         break;
 
                     case USELESS:
                         int newUselessCount = statisticsDto.getUselessFailuresSum();
-                        newUselessCount+=1;
+                        newUselessCount += 1;
                         statisticsDto.setUselessFailuresSum(newUselessCount);
                         break;
                 }
             }
         }
-
     }
-
-
-
-
-    //@Query("SELECT count (failures) from Failure failures where(failures.state = 'CLOSED' ) AND (failures.room.building.id = :buildingId)")
-  //  long countClosedFailuresByBuildingId(long buildingId);
-
 }
